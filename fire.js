f289f11227ab88d51d1e351638fdbf91e6b45a83
Patrick Levy <mrpatricklevy@gmail.com>
2016-12-23 22:17:33 -0600

    Basic game is working

---
+++ fire.js
@@ -0,0 +1,129 @@
+/******* Me Playing with previous animation**********/
+var context, time, startTime, changeTime;
+var catcherXPosition = 200;
+var catcherYPosition = 400;
+var ballX = 250;
+var ballY = 0;
+var ballSpeed = 3;
+var catcherSpeed = 20;
+var score = 0;
+var dead = 0;
+var gameIsUnderway = true;
+var img = new Image();
+
+// Begin Game
+init();
+animate();
+
+function init() {
+    //img.src = '../../lib/stickPerson.jpg';
+    //img.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+    var canvas = document.querySelector("#c");
+    context = canvas.getContext( '2d' );
+    startTime = new Date().getTime();
+
+    document.addEventListener('keydown', function(event) {
+        if (event.code === 'ArrowLeft'){
+            catcherXPosition -= catcherSpeed;
+        }
+        else if (event.code === 'ArrowRight') {
+            catcherXPosition += catcherSpeed;
+        }
+    }, false);
+}
+
+// Game loop
+function animate() {
+    /*** requestAnimationFrame ***/
+    if (gameIsUnderway) {
+        requestAnimationFrame( animate );
+    }
+
+
+    /*** processInput **/
+
+    /*** move objects and enemies ***/
+
+    // Check if the ball was caught
+    if (ballY >= catcherYPosition - 20 &&
+        ballY <= catcherYPosition + 20 &&
+        ballX >= catcherXPosition - 10 &&
+        ballX <= catcherXPosition + 55) {
+
+        console.log("score!!!");
+        score++;
+        ballY = 0;
+        ballX = _.random(50, 900);
+
+        //Increase speed every 10 points
+        if (score !== 0 && score % 10 === 0) {
+            ballSpeed++;
+        }
+    }
+
+    // Check if the ball left the screen
+    if (ballY > 475) {
+        ballY = 0;
+        ballX = _.random(50, 900);
+        console.log('death!')
+        dead++;
+    }
+
+    // If dead is greater than or equal to 10, start a new game
+    if (dead >= 5) {
+        score = 0;
+        dead = 0;
+        gameIsUnderway = false;
+    }
+
+    /*** draw the updated game ***/
+    if (gameIsUnderway) {
+        draw();
+    }
+
+}
+function draw() {
+
+    /***** Put a grey background on the screen *****/
+    context.fillStyle = 'rgb(245,245,245)';
+    context.fillRect(0, 0, 1000, 500);
+
+    /******** Draw Moving Red Dot **********/
+    ballY = ballY + ballSpeed;
+
+    // Draw the red dot at the new x and y position
+    context.fillStyle = 'rgb(255,0,0)';
+    context.beginPath();
+    context.arc(ballX, ballY, 10, 0, Math.PI * 2, true);
+    context.closePath();
+    context.fill();
+
+    /*** Draw falling stick person ***/
+    // ballY = ballY + ballSpeed;
+    //
+    // // Draw the person at the new x and y position
+    // context.drawImage(img, ballX, ballY);
+
+
+    /*********** Draw a rectangle that is controlled by the user *****/
+    context.fillStyle = 'rgb(0,0,0)';
+    context.beginPath();
+    context.strokeRect(catcherXPosition, catcherYPosition, 50, 10);
+
+    //Update scoreboard
+    context.font = "24pt Impact";
+    context.textAlign = "left";
+    context.fillStyle = "black";
+    context.fillText("Score: " + score, 50, 40);
+
+    //Update number dead
+    context.font = "24pt Impact";
+    context.textAlign = "left";
+    context.fillStyle = "black";
+    context.fillText("Dead: " + dead, 300, 40);
+}
+
+function newGame() {
+    console.log('refresh');
+    window.location.reload();
+}
